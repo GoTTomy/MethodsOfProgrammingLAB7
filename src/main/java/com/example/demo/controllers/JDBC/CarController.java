032de75e57f3c753f25db642324666d8f5af9f3e
Gottomy <igor@motowidlo.eu>
2019-11-12 20:31:11 +0100

    Trying to execute GetMapping method in JDBC

--- src/main/java/com/example/demo/controllers/JDBC/CarController.java
+++ src/main/java/com/example/demo/controllers/JDBC/CarController.java
@@ -1,7 +1,19 @@
 package com.example.demo.controllers.JDBC;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CarController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @GetMapping("getbyMark/{mark}")
+    public void findCarByMark(@PathVariable String mark){
+        String sql = "SELECT * FROM car WHERE mark ='"+ mark + "'";
+        jdbcTemplate.execute(sql);
+    }
 }
